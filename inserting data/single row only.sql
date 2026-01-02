4e50bfe6a89308177c951f26fe95a4bdaa684d79
Anamitra Bakshi <anamitra1205@gmail.com>
2026-01-03 03:23:32 +0530

    Insert a single user record into the users table

---
+++ inserting data/single row only.sql
@@ -0,0 +1,6 @@
+use wscube;
+
+INSERT INTO users
+(id, name, email, password, contact, address, dob, gender, status)
+VALUES
+(1, 'John', 'john@gmail.com', '12345678', '987654321','sayaapartmentshipra','1999-01-10', 'M', 1);
